2eae35f507841805ae83ae85ed9c09a992491b35
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2023-05-20 10:47:20 +0200

    initialize project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lightroom File Renamer Plugin
+
+## Work in progress
+
+When finished, this will add specific EXIF data to the file name of the current photo on export.
---
+++ file-renamer.lrdevplugin/Info.lua
@@ -0,0 +1,15 @@
+
+return {
+  LrSdkVersion = 5.0,
+  LrToolkitIdentifier = 'com.adobe.lightroom.sdk.fileRenamer',
+}
+
+LrExportMenuItems = {
+  title = "Rename your file",
+  file = "ExportMenuItem.lua",
+},
+
+LrLibraryMenuItems = {
+  title = "Dialog for renaming your file",
+  file = "LibraryMenuItem.lua",
+},
